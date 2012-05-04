e237f626dbdcf7059f72e71f620da9a79dc7b0ca
TheWartek <grief888@gmail.com>
2012-05-04 15:00:50 +0200

    GUI: Panel z lista algorytmow routingu, przycisk start

--- src/pl/mgrproject/RoutingAlgorithmsTester.java
+++ src/pl/mgrproject/RoutingAlgorithmsTester.java
@@ -3,10 +3,10 @@ package pl.mgrproject;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import java.awt.Toolkit;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -24,6 +24,7 @@ public class RoutingAlgorithmsTester extends JFrame {
     private JPanel graph = new GraphPanel();
     private JPanel chart = new ChartPanel();
     private JList generatorsList;
+    private JList routingAlgorithmsList;
 
     public RoutingAlgorithmsTester() {
 	initComponents();
@@ -43,19 +44,39 @@ public class RoutingAlgorithmsTester extends JFrame {
     
     private JPanel leftBar() {
 	JPanel leftBar = new JPanel();
+	leftBar.setLayout(new FlowLayout());
+	leftBar.setPreferredSize(new Dimension(180, leftBar.getHeight()));
 	leftBar.setBorder(BorderFactory.createEtchedBorder());
 	
-	//graph egenrator choice
-	String[] algorithms = new String[] {"Algorytm1", "Algorytm2", "Algorytm3", "Algorytm4", "Algorytm5", "Algorytm6",};
-	JList generatorsList = new JList(algorithms);
+	//graph generator choice
+	String[] generators = new String[] {"Generator1", "Generator2", "Generator3", "Generator4", "Generator5", "Generator6",};
+	generatorsList = new JList(generators);
 	generatorsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 	JScrollPane scroll = new JScrollPane(generatorsList);
 	scroll.setPreferredSize(new Dimension(150, 75));
+	JPanel genPanel = new JPanel();
+	genPanel.setLayout(new FlowLayout());
+	genPanel.setBorder(BorderFactory.createTitledBorder("Generatory grafu"));
+	genPanel.add(scroll);
+	leftBar.add(genPanel);
+	//***//
+	
+	//routing algorithm choice
+	String[] routingAlgorithms = new String[] {"Algorytm1", "Algorytm2", "Algorytm3", "Algorytm4", "Algorytm5", "Algorytm6",};
+	routingAlgorithmsList = new JList(routingAlgorithms);
+	routingAlgorithmsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+	scroll = new JScrollPane(routingAlgorithmsList);
+	scroll.setPreferredSize(new Dimension(150, 75));
 	JPanel algPanel = new JPanel();
 	algPanel.setLayout(new FlowLayout());
-	algPanel.setBorder(BorderFactory.createTitledBorder("Generatory grafu"));
+	algPanel.setBorder(BorderFactory.createTitledBorder("Algorytmy routingu"));
 	algPanel.add(scroll);
 	leftBar.add(algPanel);
+	//***//
+	
+	//start button//
+	JButton start = new JButton("Start");
+	leftBar.add(start);
 	//***//
 	
 	return leftBar;

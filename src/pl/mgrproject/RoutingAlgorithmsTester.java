0436c1b1bb14a7db2d7b0fc86ebdf7b86a9bb33b
TheWartek <grief888@gmail.com>
2012-05-04 14:30:37 +0200

    Dodany komponent do listy generatorow, splitPane dla komponentow rysujacych

--- src/pl/mgrproject/RoutingAlgorithmsTester.java
+++ src/pl/mgrproject/RoutingAlgorithmsTester.java
@@ -1,12 +1,18 @@
 package pl.mgrproject;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 
 import pl.mgrproject.components.ChartPanel;
@@ -17,6 +23,7 @@ public class RoutingAlgorithmsTester extends JFrame {
     private static final long serialVersionUID = -9006966197122760691L;
     private JPanel graph = new GraphPanel();
     private JPanel chart = new ChartPanel();
+    private JList generatorsList;
 
     public RoutingAlgorithmsTester() {
 	initComponents();
@@ -26,16 +33,31 @@ public class RoutingAlgorithmsTester extends JFrame {
 	setLayout(new BorderLayout());
 	add(BorderLayout.WEST, leftBar());
 	JPanel main = new JPanel();
-	main.setLayout(new GridLayout(2, 1));
-	main.add(graph);
-	main.add(chart);
+	main.setLayout(new BorderLayout());
+	JSplitPane split = new JSplitPane(JSplitPane.VERTICAL_SPLIT, graph, chart);
+	split.setOneTouchExpandable(true);
+	split.setDividerLocation(Toolkit.getDefaultToolkit().getScreenSize().height / 2);
+	main.add(split);
 	add(main);
     }
     
     private JPanel leftBar() {
 	JPanel leftBar = new JPanel();
-	leftBar.add(new JLabel("Left Bar"));
 	leftBar.setBorder(BorderFactory.createEtchedBorder());
+	
+	//graph egenrator choice
+	String[] algorithms = new String[] {"Algorytm1", "Algorytm2", "Algorytm3", "Algorytm4", "Algorytm5", "Algorytm6",};
+	JList generatorsList = new JList(algorithms);
+	generatorsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+	JScrollPane scroll = new JScrollPane(generatorsList);
+	scroll.setPreferredSize(new Dimension(150, 75));
+	JPanel algPanel = new JPanel();
+	algPanel.setLayout(new FlowLayout());
+	algPanel.setBorder(BorderFactory.createTitledBorder("Generatory grafu"));
+	algPanel.add(scroll);
+	leftBar.add(algPanel);
+	//***//
+	
 	return leftBar;
     }
     

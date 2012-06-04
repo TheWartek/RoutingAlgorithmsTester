9307e066f0e301ba058c4d4bd74c9ec398cf4647
TheWartek <grief888@gmail.com>
2012-06-04 22:37:54 +0200

    Wylosowanie wierzcholka startowego i koncowego i uruchomienie algorytmu routingu.

--- src/pl/mgrproject/controller/StepAction.java
+++ src/pl/mgrproject/controller/StepAction.java
@@ -1,12 +1,16 @@
 package pl.mgrproject.controller;
 
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.Random;
 
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 
 import pl.mgrproject.api.Environment;
+import pl.mgrproject.api.Graph;
 import pl.mgrproject.api.plugins.Generator;
 import pl.mgrproject.api.plugins.RoutingAlgorithm;
 
@@ -38,7 +42,17 @@ public class StepAction implements ActionListener {
 	}
 	
 	Environment.drawStep(generator);
-	algorithm.setGraph(generator.getGraph());
+	Graph<?> g = generator.getGraph();
+	int n = g.getVertices().size();
+	algorithm.setGraph(g);
+	Random rand = new Random();
+	int start = rand.nextInt(n);
+	int stop;
+	do {
+	    stop = rand.nextInt(n);
+	} while (stop == start);
+	algorithm.run(start);
+	List<Point> path = algorithm.getPath(stop);
     }
 
 }
